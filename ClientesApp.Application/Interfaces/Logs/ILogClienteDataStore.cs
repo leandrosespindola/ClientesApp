2d9695d03eb1d0e6a7c521ab72cf4135a1d06ec5
leandrosespindola <leandro.s.espindola@gmail.com>
2024-10-09 21:45:47 -0300

    Criada consulta no mongo.

--- ClientesApp.Application/Interfaces/Logs/ILogClienteDataStore.cs
+++ ClientesApp.Application/Interfaces/Logs/ILogClienteDataStore.cs
@@ -5,6 +5,7 @@ namespace ClientesApp.Application.Interfaces.Logs
     public interface ILogClienteDataStore
     {
         Task AddAsync(LogClienteModel model);
-        Task<List<LogClienteModel>> Get(Guid clienteId, int pageNumber, int pageSize);
+        Task<List<LogClienteModel>> GetAsync(Guid clienteId, int pageNumber, int pageSize);
+        Task<int> GetTotalCountAsync(Guid clienteId);
     }
 }
--- ClientesApp.Infra.Data.MongoDB/Storages/LogClienteDataStore.cs
+++ ClientesApp.Infra.Data.MongoDB/Storages/LogClienteDataStore.cs
@@ -1,6 +1,7 @@
 ﻿using ClientesApp.Application.Interfaces.Logs;
 using ClientesApp.Application.Models;
 using ClientesApp.Infra.Data.MongoDB.Contexts;
+using MongoDB.Driver;
 
 namespace ClientesApp.Infra.Data.MongoDB.Storages
 {
@@ -16,6 +17,27 @@ namespace ClientesApp.Infra.Data.MongoDB.Storages
         public async Task AddAsync(LogClienteModel model)
         {
             await _mongoDBContext.LogClientes.InsertOneAsync(model);
+        }
+
+        public async Task<List<LogClienteModel>> GetAsync(Guid clienteId, int pageNumber, int pageSize)
+        {
+            var filter = Builders<LogClienteModel>.Filter.Eq(log => log.ClienteId, clienteId);
+
+            var result = await _mongoDBContext.LogClientes
+                .Find(filter)
+                .Skip((pageNumber -1) * pageSize)
+                .Limit(pageSize)
+                .SortByDescending(log => log.DataOperacao)
+                .ToListAsync();
+
+            return result;
+        }
+
+        public async Task<int> GetTotalCountAsync(Guid clienteId)
+        {
+            var filter = Builders<LogClienteModel>.Filter.Eq(log => log.ClienteId, clienteId);
+
+            return (int) await _mongoDBContext.LogClientes.CountDocumentsAsync(filter);
         }
     }
 }
